3bd88aa4803c32747253493d6ba3972ab4612115
Abdullah <a.hammad22@hotmail.com>
2026-05-26 16:24:07 +0300

    Connected Spring Boot to MySQL

---
+++ src/main/java/com/abdullah/taskmanager/model/Task.java
@@ -0,0 +1,28 @@
+package com.abdullah.taskmanager.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
